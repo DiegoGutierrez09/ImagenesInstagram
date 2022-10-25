c14d4edb4871c031655bbe485ee3b56924ce829d
DiegoGutierrez09 <gutierrezdiego2730@gmail.com>
2022-10-24 21:25:27 -0500

    Se agrega ruta especifica de descarga

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
-# This is a sample Python script.
 
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 import pandas as pd
 import time
 
 
+chromeOptions = Options()
+chromeOptions.add_experimental_option("prefs",{
+    "download.default_directory" : "D:\Diego\Documents\TESIS\Images",
+})
 
 def download_insta(link):
-    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver = webdriver.Chrome(ChromeDriverManager().install(),chrome_options=chromeOptions)
     driver.get("https://downloadgram.org/")
     text_box = driver.find_element("name", "url")
     text_box.send_keys(link)
 
     driver.find_element(By.ID, value='submit').click()
-    time.sleep(5)
+    time.sleep(1)
     try:
         driver.find_element(By.LINK_TEXT, value='DOWNLOAD').click()
         time.sleep(1)
@@ -32,5 +35,5 @@ links_file = pd.read_csv('LinkTesis.csv')
 links = links_file['Links']
 
 for link in links:
-    print(link)
+    #print(link)
     download_insta(link)
